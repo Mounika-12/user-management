782aeb3df35e5b5436ff13eeb57aea95ed37e994
mounika <mounika.kommineni12@gmail.com>
2021-05-31 15:59:01 +0530

    added pagination and few customizations

--- user_management/users_data/serializers.py
+++ user_management/users_data/serializers.py
@@ -2,11 +2,34 @@ from rest_framework import serializers
 from .models import *
 from drf_queryfields import QueryFieldsMixin
 
-
-
 class usersSerializer(QueryFieldsMixin,serializers.ModelSerializer):
 
     class Meta:
         model = Users
         fields = '__all__'
+
+    # Custom validation can be performed on the fields
+    def validate(self, data):
+        """
+        Check that input data and raise validation errors
+        """
+        return data
+
+    # Customize the user data before saving to database
+    def create(self, validated_data):
+
+        users = Users.objects.create(**validated_data)
+
+        return users
+
+    # Customize the user data before updating and saving to database
+    def update(self, instance, validated_data):
+
+        for item in validated_data:
+            setattr(instance, item, validated_data[item])
+
+        instance.save()
+        return instance
+
+
 
